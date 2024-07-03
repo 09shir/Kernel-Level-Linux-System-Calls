37da170892c39bc80315b5edcc056b13b35a9ac9
Roy <89437956+09shir@users.noreply.github.com>
2024-07-03 00:35:34 -0700

    use array_stats struct

--- cmpt300/array_stats.c
+++ cmpt300/array_stats.c
@@ -4,7 +4,8 @@
 #include "array_stats.h"
 
 SYSCALL_DEFINE3(array_stats, struct array_stats __user *, stats, long __user *, data, long, size) {
-    long min, max, sum, val;
+    struct array_stats kernel_stats;
+    long val;
     int i;
 
     if (size <= 0)
@@ -13,25 +14,25 @@ SYSCALL_DEFINE3(array_stats, struct array_stats __user *, stats, long __user *, 
     if (!access_ok(stats, sizeof(struct array_stats)) || !access_ok(data, size * sizeof(long)))
         return -EFAULT;
 
+    // Perform the first data read:
     if (copy_from_user(&val, &data[0], sizeof(long)))
         return -EFAULT;
 
-    min = max = sum = val;
+    kernel_stats.min = kernel_stats.max = kernel_stats.sum = val;
 
     for (i = 1; i < size; i++) {
         if (copy_from_user(&val, &data[i], sizeof(long)))
             return -EFAULT;
 
-        if (val < min)
-            min = val;
-        if (val > max)
-            max = val;
-        sum += val;
+        if (val < kernel_stats.min)
+            kernel_stats.min = val;
+        if (val > kernel_stats.max)
+            kernel_stats.max = val;
+        kernel_stats.sum += val;
     }
 
-    if (copy_to_user(&stats->min, &min, sizeof(long)) ||
-        copy_to_user(&stats->max, &max, sizeof(long)) ||
-        copy_to_user(&stats->sum, &sum, sizeof(long)))
+    // Copy entire struct at once:
+    if (copy_to_user(stats, &kernel_stats, sizeof(struct array_stats)))
         return -EFAULT;
 
     return 0;
